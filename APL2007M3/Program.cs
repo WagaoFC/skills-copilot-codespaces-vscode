93ea766f69131f0a782a20d9296d62fade5753fc
Wagner Ferreirar Costa <wagnerferreira_07@hotmail.com>
2024-10-21 19:15:48 +0000

    finishing exercise module /build-auto-suggest-engine-copilot

--- APL2007M3/Program.cs
+++ APL2007M3/Program.cs
@@ -129,4 +129,216 @@ namespace ReportGenerator
 
         return randomDate;
     }
+
+    string[] words = {
+        "as", "astronaut", "asteroid", "are", "around",
+        "cat", "cars", "cares", "careful", "carefully",
+        "for", "follows", "forgot", "from", "front",
+        "mellow", "mean", "money", "monday", "monster",
+        "place", "plan", "planet", "planets", "plans",
+        "the", "their", "they", "there", "towards"
+    };
+    
+    Trie dictionary = InitializeTrie(words);
+    // SearchWord();
+    // PrefixAutocomplete();
+    // DeleteWord();
+    // GetSpellingSuggestions();
+
+    // Inicializa a Trie com uma lista de palavras
+    Trie InitializeTrie(string[] words)
+    {
+        Trie trie = new Trie();
+
+        foreach (string word in words)
+        {
+            trie.Insert(word);
+        }
+
+        return trie;
+    }
+    
+    // Permite ao usuário buscar uma palavra na Trie
+    void SearchWord()
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter a word to search for, or press Enter to exit.");
+            string? input = Console.ReadLine();
+            if (input == "")
+            {
+                break;
+            }
+            /*
+            if (input != null && dictionary.Search(input))
+            {
+                Console.WriteLine($"Found \"{input}\" in dictionary");
+            }
+            */
+            else
+            {
+                Console.WriteLine($"Did not find \"{input}\" in dictionary");
+            }
+        }
+    }
+    
+    // Permite ao usuário buscar palavras por prefixo na Trie
+    void PrefixAutocomplete()
+    {
+        PrintTrie(dictionary);
+        GetPrefixInput();
+    }
+
+    // Permite ao usuário deletar uma palavra da Trie
+    void DeleteWord() 
+    {
+        PrintTrie(dictionary);
+        while(true)
+        {
+            Console.WriteLine("\nEnter a word to delete, or press Enter to exit.");
+            string? input = Console.ReadLine();
+            if (input == "")
+            {
+                break;
+            }
+            /*
+            if (input != null && dictionary.Search(input))
+            {
+                dictionary.Delete(input);
+                Console.WriteLine($"Deleted \"{input}\" from dictionary\n");
+                PrintTrie(dictionary);
+            }
+            */
+            else
+            {
+                Console.WriteLine($"Did not find \"{input}\" in dictionary");
+            }
+        }
+    }
+    
+    // Permite ao usuário obter sugestões de ortografia para uma palavra
+    void GetSpellingSuggestions() 
+    {
+        PrintTrie(dictionary);
+        Console.WriteLine("\nEnter a word to get spelling suggestions for, or press Enter to exit.");
+        string? input = Console.ReadLine();
+        if (input != null)
+        {
+            var similarWords = dictionary.GetSpellingSuggestions(input);
+            Console.WriteLine($"Spelling suggestions for \"{input}\":");
+            if (similarWords.Count == 0)
+            {
+                Console.WriteLine("No suggestions found.");
+            }
+            else 
+            {
+                foreach (var word in similarWords)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+    
+    // Executa todos os métodos de exemplo
+    void RunAllExercises()
+    {
+        SearchWord();
+        PrefixAutocomplete();
+        DeleteWord();
+        GetSpellingSuggestions();
+    }
+
+    // Obtém a entrada do usuário para buscar palavras por prefixo
+    void GetPrefixInput()
+    {
+        Console.WriteLine("\nEnter a prefix to search for, then press Tab to " + "cycle through search results. Press Enter to exit.");
+
+        bool running = true;
+        string prefix = "";
+        StringBuilder sb = new StringBuilder();
+        List<string>? words = null;
+        int wordsIndex = 0;
+
+        while(running)
+        {
+            var input = Console.ReadKey(true);
+
+            if (input.Key == ConsoleKey.Spacebar)
+            {
+                Console.Write(' ');
+                prefix = "";
+                sb.Append(' ');
+                continue;
+            } 
+            else if (input.Key == ConsoleKey.Backspace && Console.CursorLeft > 0)
+            {
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                Console.Write(' ');
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+
+                sb.Remove(sb.Length - 1, 1);
+                prefix = sb.ToString().Split(' ').Last();
+            }
+            else if (input.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                running = false;
+                continue;
+            }
+            else if (input.Key == ConsoleKey.Tab && prefix.Length > 1)
+            {
+                string previousWord = sb.ToString().Split(' ').Last();
+
+                if (words != null) {
+                    if (!previousWord.Equals(words[wordsIndex - 1]))
+                    {
+                        words = dictionary.AutoSuggest(prefix);
+                        wordsIndex = 0;
+                    }
+                } 
+                else {
+                    words = dictionary.AutoSuggest(prefix);
+                    wordsIndex = 0;
+                }
+
+                for (int i = prefix.Length; i < previousWord.Length; i++)
+                {
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    Console.Write(' ');
+                    Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                    sb.Remove(sb.Length - 1, 1);
+                }
+            
+                
+                if (words.Count > 0 && wordsIndex < words.Count)
+                {
+                    string output = words[wordsIndex++];
+                    Console.Write(output.Substring(prefix.Length));
+                    sb.Append(output.Substring(prefix.Length));
+                }
+                continue;
+            }
+            else if (input.Key != ConsoleKey.Tab)
+            {
+                Console.Write(input.KeyChar);
+                prefix += input.KeyChar;
+                sb.Append(input.KeyChar);
+                words = null;
+                wordsIndex = 0;
+            }
+        }
+    }
+
+    // Imprime todas as palavras contidas na Trie
+    void PrintTrie(Trie trie)
+    {
+        Console.WriteLine("The dictionary contains the following words:");
+        List<string> words = trie.GetAllWords();
+        foreach (string word in words)
+        {
+            Console.Write($"{word}, ");
+        }
+        Console.WriteLine();
+    }
 }
